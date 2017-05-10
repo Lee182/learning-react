35f74a8b217b259b5029f965a67e8ac2e907c6a1
Jonathan T L Lee <jono-lee@hotmail.co.uk>
2017-05-10 21:47:56 +0100

    Completed HelloReact
    was a basic copy and paste setup

---
+++ learnyoureact/views/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class TodoBox extends React.Component{
+  render() {
+    return <div className="todoBox">
+      Hello, world!
+    </div>
+  }
+}
+
+export default TodoBox
