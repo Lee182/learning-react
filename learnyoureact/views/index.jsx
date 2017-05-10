3568b11a5662f8ecc8821866c32a84b3818968c1
Jonathan T L Lee <jono-lee@hotmail.co.uk>
2017-05-10 22:02:19 +0100

    Completed Components

--- learnyoureact/views/index.jsx
+++ learnyoureact/views/index.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 
-class TodoBox extends React.Component{
+class TodoBox extends React.Component {
   render() {
-    return <div className="todoBox">
-      Hello, world!
-    </div>
+    return (
+      <div className="todoBox">
+          <h1>Todos</h1>
+          <TodoList />
+          <TodoForm />
+      </div>
+    );
+  }
+}
+
+class TodoList extends React.Component {
+  render() {
+    return (<div className="todoList">I am a TodoList.</div>)
+  }
+}
+
+class TodoForm extends React.Component {
+  render() {
+    return (<div className="todoForm">I am a TodoForm.</div>)
   }
 }
 
