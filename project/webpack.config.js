a71df58df816d9727d17f49ef6baa97509b9ed84
Jonathan T L Lee <jono-lee@hotmail.co.uk>
2017-05-14 09:03:34 +0100

    simplified server loading code

--- project/webpack.config.js
+++ project/webpack.config.js
@@ -1,31 +1,30 @@
+// learncodeacademy/react-js-tutorials/6-mobx-react
+var debug = process.env.NODE_ENV !== "production";
 var webpack = require('webpack');
+var path = require('path');
 
 module.exports = {
-  devtool: 'inline-source-map',
-  entry: [
-    'webpack-hot-middleware/client',
-    './client/client.js',
-  ],
-  output: {
-    path: require("path").resolve("./dist"),
-    filename: 'bundle.js',
-    publicPath: '/'
-  },
-  plugins: [
-    new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
-  ],
+  context: path.join(__dirname),
+  devtool: debug ? "inline-sourcemap" : null,
+  entry: "./client/client.js",
   module: {
     loaders: [
       {
         test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
         loader: 'babel-loader',
-        exclude: /node_modules/,
-        query: {
-          presets: ['react', 'es2015', 'react-hmre']
-        }
-      }
+        query: { presets: ['es2015', 'react'], plugins: ["transform-decorators-legacy", "transform-class-properties"] }
+      },
+      { test: /\.styl$/, loader: "stylus-loader" },
     ]
-  }
-}
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  plugins: debug ? [] : [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+  ],
+};
